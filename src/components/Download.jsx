1032038bd51a6692938d33b07497ce15899acbcc
eldiablood23 <moneyemoji.pro@gmai.com>
2022-10-16 15:37:11 +0200

    first commit

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -8,11 +8,11 @@ const Download = () => {
     <div className={`${styles.section} ${styles.bgWhite}`}>
       <div className={`${styles.subSection} flex-col text-center`}>
         <div>
-          <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the Source Code</h1>
-          <p className={`${styles.pText} ${styles.blackText}`}>Get the full source code on GitHub</p>
+          <h1 className={`${styles.h1Text} ${styles.blackText}`}>Stáhnout zdrojový kód</h1>
+          <p className={`${styles.pText} ${styles.blackText}`}>Získej zdrojový kód na Github</p>
         </div>
         <a href="https://github.com/eldiabloodjr23/NftMarket" target="_blank" rel="noreferrer">
-        <button className={styles.btnPrimary}>Source Code</button>
+        <button className={styles.btnPrimary}>Zdrojový kód</button>
         </a>
         <div className={styles.flexCenter}>
           <img 
