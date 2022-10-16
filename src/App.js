9648a97fb454c13e5bcacb00942e8a6ef989c632
eldiablood23 <moneyemoji.pro@gmai.com>
2022-10-16 15:29:51 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -22,13 +22,13 @@ const App = () => {
       <Features />
       <SectionWrapper 
         title="Deployment"
-        description="ProNef is built using Expo which runs natively on all users' devices. You can easily get your app into people's hands"
+        description="ProNef je vytvořen pomocí Expo, které běží nativně na zařízeních všech uživatelů. Svou aplikaci můžete snadno dostat do rukou lidí"
         mockupImg={assets.feature}
         reverse
       />
       <SectionWrapper 
-        title="Creative way to showcase the store"
-        description="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
+        title=" Kreativní cesta jak ukázat svůj obchod"
+        description="Aplikace obsahuje dvě obrazovky. První obrazovka obsahuje seznam všech NFT, zatímco druhá zobrazuje podrobnosti o konkrétním NFT." 
         mockupImg={assets.mockup}
         banner="banner02"
       />
@@ -36,7 +36,7 @@ const App = () => {
 
       <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
         <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by {" "}
-        <span className="bold">Kamil El-Achheb</span>
+        <span className="bold">weinvent</span>
         </p>
       </div>
     </>
